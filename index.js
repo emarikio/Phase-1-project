48e882b816209ec938f189b16e5d3d6b2c7846c8
Emarilyne Wakio <emarilynew@gmail.com>
2024-04-11 13:57:46 +0300

    Daily project app

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 function submitJournal(event) {
     event.preventDefault();
 
-    const mood = document.getElementById('Mood-selector');
-    const day = document.getElementById('day-selector');
-    const week = document.getElementById('Week-selector');
-    const entry = document.getElementById('entry');
+    const mood = document.getElementById('mood').value;
+    const day = document.getElementById('day-selector').value;
+    const week = document.getElementById('Week-selector').value;
+    const entry = document.getElementById('entry').value;
+
+    
 
     let journalSubmit = {
         mood: mood,
@@ -13,17 +15,14 @@ function submitJournal(event) {
         week: day,
         entry: entry
     };
-    
-    
-    
 
     // Reset the form fields after submission
-    document.getElementById('mood').selectedIndex = 0;
-    document.getElementById('day-selector').selectedIndex = 0;
-    document.getElementById('Week-selector').selectedIndex = 0;
-    document.getElementById('entry').selectedIndex = ''; // => This clears the form after submission
-
+document.getElementById('mood').selectedIndex = 0;
+document.getElementById('day-selector').selectedIndex = 0;
+document.getElementById('Week-selector').selectedIndex = 0;
+document.getElementById('entry').selectedIndex = ''; // => This clears the form after submission
 }
+
 // Event listener for the submission above
 document.getElementById('journal-form').addEventListener('submit', submitJournal);
 
@@ -37,8 +36,8 @@ function getQuote() {
     .then((data) => {
         return(data);
     });
-let randomIndex = [Math.floor(Math.random() * quote.length)];
-alert(quote); // => quote will show as an alert
+    let randomIndex = [Math.floor(Math.random() * getQuote.length)];
+    alert(quote);
 }
 
 document.getElementById('quote').addEventListener('click', getQuote); // => This  is the event listener for the Quote Button
